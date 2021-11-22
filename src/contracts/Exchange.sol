dbca46b1b9b866ffb5ce22da4efc0189f18cb37e
avk393 <ankithk@utexas.edu>
2021-11-21 21:33:18 -0600

    exchange completed

--- src/contracts/Exchange.sol
+++ src/contracts/Exchange.sol
@@ -11,9 +11,26 @@ contract Exchange {
     uint256 public feePercent;
     address constant ETHER = address(0);    // store Ether in tokens mapping with empty adress
     mapping(address => mapping(address => uint256)) public tokens;
+    mapping(uint256 => _Order) public orders;
+    uint256 public orderCount;
+    mapping(uint256 => bool) public orderCancelled;
+    mapping(uint256 => bool) public orderFilled;
 
     event Deposit(address token, address user, uint256 amount, uint256 balance);
     event Withdraw(address token, address user, uint256 amount, uint256 balance);
+    event Order(uint256 id, address user, address tokenGet, uint256 amountGet, address tokenGive, uint256 amountGive, uint256 timestamp);
+    event Cancel(uint256 id, address user, address tokenGet, uint256 amountGet, address tokenGive, uint256 amountGive, uint256 timestamp);
+    event Trade(uint256 id, address user, address tokenGet, uint256 amountGet, address tokenGive, uint256 amountGive, uint256 timestamp);
+
+    struct _Order {
+        uint    id;
+        address user;
+        address tokenGet;
+        uint    amountGet;
+        address tokenGive;
+        uint    amountGive;
+        uint    timestamp;
+    }
 
     constructor(address _feeAccount, uint256 _feePercent) public {
         feeAccount = _feeAccount;
@@ -52,5 +69,40 @@ contract Exchange {
 
     function balanceOf(address _token, address _user) public view returns(uint256){
         return tokens[_token][_user];
+    }
+
+    function makeOrder(address _tokenGet, uint256 _amountGet, address _tokenGive, uint256 _amountGive) public {
+        orderCount++;
+        orders[orderCount] = _Order(orderCount, msg.sender, _tokenGet, _amountGet, _tokenGive, _amountGive, now);
+        emit Order(orderCount, msg.sender, _tokenGet, _amountGet, _tokenGive, _amountGive, now);
+    }
+
+    function cancelOrder(uint256 _id) public {
+        // Require user who created order to cancel order
+        _Order storage _order = orders[_id];
+        require(address(_order.user) == msg.sender);
+        require(_order.id == _id);
+        orderCancelled[_id] = true;
+        emit Cancel(_order.id, _order.user, _order.tokenGet, _order.amountGet, _order.tokenGive, _order.amountGive, _order.timestamp);
+    }
+
+    function _trade(uint256 _id, address _user, address _tokenGet, uint256 _amountGet, address _tokenGive, uint256 _amountGive) internal {
+        uint256 _feeAmount = (_amountGet * feePercent) / 100;
+        
+        tokens[_tokenGet][_user] = tokens[_tokenGet][_user] + (_amountGet);
+        tokens[_tokenGet][msg.sender] = tokens[_tokenGet][msg.sender] - (_amountGet + _feeAmount);
+        tokens[_tokenGet][feeAccount] += _feeAmount;
+        tokens[_tokenGive][_user] -= _amountGive;
+        tokens[_tokenGive][msg.sender] += _amountGive;
+        emit Trade(_id, _user, _tokenGet, _amountGet, _tokenGive, _amountGive, now);
+    }
+
+    function fillOrder(uint256 _id) public {
+        require(_id>0 && _id<=orderCount);
+        require(!orderFilled[_id]);
+        require(!orderCancelled[_id]);
+        _Order storage _order = orders[_id];
+        _trade(_order.id, _order.user, _order.tokenGet, _order.amountGet, _order.tokenGive, _order.amountGive);
+        orderFilled[_order.id] = true;
     }
 }
--- test/Exchange.test.js
+++ test/Exchange.test.js
@@ -9,7 +9,7 @@ require('chai')
     .use(require('chai-as-promised'))
     .should()
 
-contract('Exchange', ([deployer, feeAccount, user1]) => {
+contract('Exchange', ([deployer, feeAccount, user1, user2]) => {
     let token
     let exchange
     const feePercent = 10
@@ -163,5 +163,67 @@ contract('Exchange', ([deployer, feeAccount, user1]) => {
             
         })
     })
-    
+
+    describe('making order', ()=> {
+        let result
+        let amount = tokens(1)
+
+        beforeEach(async() => {
+            result = await exchange.makeOrder(token.address, amount, ETHER_ADDRESS, ether(1), {from: user1})
+        })
+
+        it('tracks the newly created order', async() => {
+            const orderCount = await exchange.orderCount()
+            orderCount.toString().should.equal('1', 'order coount is correct')
+            const order = await exchange.orders('1')
+            order.id.toString().should.equal('1', 'id is correct')
+            order.user.should.equal(user1)
+            order.tokenGet.should.equal(token.address)
+            order.amountGet.toString().should.equal(amount.toString())
+            order.tokenGive.should.equal(ETHER_ADDRESS)
+            order.amountGive.toString().should.equal(ether(1).toString())
+            order.timestamp.toString().length.should.be.at.least(1)
+        })
+    })
+
+    describe('order actions', () => {
+        beforeEach(async() => {
+            // user1 deposits ether only
+            await exchange.depositEther({from: user1, value: ether(1)})
+            // give tokens to user2
+            await token.transfer(user2, tokens(100), {from: deployer})
+            // user2 deposits tokens
+            await token.approve(exchange.address, tokens(2), {from: user2})
+            await exchange.depositToken(token.address, tokens(2), {from: user2})
+            // user1 makes an order to swap ETHER for token
+            await exchange.makeOrder(token.address, tokens(1), ETHER_ADDRESS, ether(1), {from: user1})
+        })
+
+        describe('filling orders', async() => {
+            let result
+
+            describe('success', async() => {
+                beforeEach(async() => {
+                    result = await exchange.fillOrder('1', {from: user2})
+                })
+                
+                it('executes trades & charges fees', async() => {
+                    let balance
+                    balance = await exchange.balanceOf(token.address, user1)
+                    balance.toString().should.equal(tokens(1).toString(), 'user1 received tokens')
+                    balance = await exchange.balanceOf(ETHER_ADDRESS, user2)
+                    balance.toString().should.equal(ether(1).toString(), 'user2 received tokens')
+                    balance = await exchange.balanceOf(ETHER_ADDRESS, user1)
+                    balance.toString().should.equal('0', 'user2 ETH deducted')
+                    balance = await exchange.balanceOf(token.address, user2)
+                    balance.toString().should.equal(tokens(0.9).toString(), 'user2 tokens deducted with fee applied')
+                    balance = await exchange.balanceOf(token.address, feeAccount)
+                    balance.toString().should.equal(tokens(0.1).toString(), 'fee received')
+                })
+            })
+
+
+        })
+    })
+
 })
